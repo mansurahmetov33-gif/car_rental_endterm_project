746b97ef5c1de51d2c8dfb200ae7ea605e96e191
userp <mansurahmetov3@gmail.com>
2026-02-07 19:07:51 +0500

    feat: add DTO

---
+++ src/main/java/org/carrental/DTO/CarRequestDTO.java
@@ -0,0 +1,29 @@
+package org.carrental.DTO;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public class CarRequestDTO {
+
+    @NotBlank(message = "Name must not be empty")
+    private String name;
+
+    @Min(value = 1, message = "Price must be greater than 0")
+    private double pricePerDay;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+}
